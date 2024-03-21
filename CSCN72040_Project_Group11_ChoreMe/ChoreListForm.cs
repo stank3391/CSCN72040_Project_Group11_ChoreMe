a50e7abd863039a902bb9249633be1b0eacbfd6d
Justin Schulz <jschulz8138@conestogac.on.ca>
2024-03-20 20:13:04 -0400

    Added blank forms

---
+++ CSCN72040_Project_Group11_ChoreMe/ChoreListForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSCN72040_Project_Group11_ChoreMe
+{
+    public partial class ChoreListForm : Form
+    {
+        public ChoreListForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
