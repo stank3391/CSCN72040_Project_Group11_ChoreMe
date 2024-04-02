38f9f49ec532c272efade8ec64ce8e63dcabde00
Bowen Sittler <bsittler9691@conestogac.on.ca>
2024-04-01 21:16:00 -0400

    added observer pattern implementation

---
+++ CSCN72040_Project_Group11_ChoreMe/ConcreteObserver.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCN72040_Project_Group11_ChoreMe
+{
+    // Concrete observer
+    public class AlertBoxObserver : IChoreObserver
+    {
+        public void NotifyChoreCreated(string message)
+        {
+            MessageBox.Show(message, "Chore Created", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
