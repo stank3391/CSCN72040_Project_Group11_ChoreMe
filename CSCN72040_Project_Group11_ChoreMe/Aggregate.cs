d2dc75e393ec92606a04a98361c20f3262501e84
Sean <stank3391@conestogac.on.ca>
2024-04-01 21:25:19 -0400

    Added Aggregate interface and corelist interator

---
+++ CSCN72040_Project_Group11_ChoreMe/Aggregate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChoreMe;
+
+namespace CSCN72040_Project_Group11_ChoreMe
+{
+    public interface Aggregate<T>
+    {
+        Iterator<T> createIterator(IteratorType type);
+    }
+}
---
+++ CSCN72040_Project_Group11_ChoreMe/ChoreListIterator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChoreMe;
+
+namespace CSCN72040_Project_Group11_ChoreMe
+{
+    internal class ChoreListIterator : Iterator<Chore>
+    {
+        private List<Chore> list;
+        int current = 0;
+        ChoreListIterator (List<Chore> list)
+        {
+            this.list = list;
+        }
+
+        public bool hasNext()
+        {
+            return current < list.Count;
+        }
+
+        public Chore Next()
+        {
+            return list[current++];
+        }
+    }
+}
